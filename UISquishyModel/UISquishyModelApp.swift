9abfea7949f055ca8abc306b6915c788c5fa8c98
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-09 23:52:36 -0300

    Initial Commit

---
+++ UISquishyModel/UISquishyModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISquishyModelApp.swift
+//  UISquishyModel
+//
+//  Created by Luiz Araujo on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UISquishyModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
